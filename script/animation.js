94e47dce10458e27572e5490da4ff38ee1d59f5e
Tolfx <matteus12122@gmail.com>
2020-09-22 17:53:42 +0200

    🎈Animation

--- script/animation.js
+++ script/animation.js
@@ -1,9 +1,20 @@
 const powerOut_One = gsap.timeline({ defaults: { ease: "power1.out" } });
 
 //Coming soon container
+
 powerOut_One.to(
   ".container",
   { y: "-50%", duration: 1, stagger: 0.25 },
+);
+powerOut_One.fromTo(
+  ".flexbox-container",
+  { opacity: 0 },
+  { opacity: 1, duration: 0.5 },
+);
+powerOut_One.fromTo(
+  "#about",
+  { opacity: 0 },
+  { opacity: 1, duration: 0.5 },
 );
 
 powerOut_One.fromTo(
