735734998af0f94cac77c04504ee5723182224bd
Tolfx <matteus12122@gmail.com>
2020-09-23 10:30:39 +0200

    🎈Big commit

--- script/getInformation.js
+++ script/getInformation.js
@@ -1,4 +1,6 @@
 const URLGIT = "https://api.github.com/users/Tolfx";
+const email = "matteus@tolfx.dev";
+const twitterURL = "https://twitter.com/tolfx";
 const flexbox_DOM = document.querySelector(".flexbox");
 
 const fetchUser = async () => {
@@ -9,8 +11,37 @@ const fetchUser = async () => {
 
 function printInformation() {
   fetchUser().then((data) => {
-    flexbox_DOM.innerHTML =
-      `URL: ${data.html_url} <br/> Repos: ${data.public_repos} <br/> Name: ${data.login} <br/> Location: ${data.location} <br/> <br/> This was automated by fetching from github`;
+    const text = `
+    About Me:
+    <br/>
+    Name: Matteus
+    <br/>
+    Age: 17
+    <br/>
+    Country: Sweden
+    <br/>
+    <br/>
+    Github Information: 
+    <br/>
+    <a href="${data.html_url}">Link to github</a>
+    <br/> 
+    Repos: ${data.public_repos} 
+    <br/> 
+    Name: ${data.login} 
+    <br/> 
+    Location: ${data.location} 
+    <br/> 
+    <br/> 
+    <a id="contacts">Contacts</a>:<br/>
+    Email: ${email}
+    <br/>
+    Twitter: <a href="${twitterURL}">@Tolfx</a>
+    <br/>
+    <br/>
+    Surveys: <br/>
+    Coming soon...
+    `;
+    flexbox_DOM.innerHTML = text;
   });
 }
 
